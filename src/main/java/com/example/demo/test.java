95ae043dc7a3eeff2fb0054cc36b885d1c09d699
charlielin123 <lingd123@gmail.com>
2023-03-05 03:30:38 +0800

    asd

--- src/main/java/com/example/demo/test.java
+++ src/main/java/com/example/demo/test.java
@@ -1,14 +1,13 @@
-package com.awsdemo.demo.Controller;
+package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class testController {
-
+public class test {
+    
     @GetMapping("/hello")
     public String name() {
-        return "Hello de non";
+        return " skldflskadfjasdf";
     }
-
 }
