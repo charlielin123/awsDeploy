dde8baf5103ca9a39ea3ed53adb65268fb10f852
charlielin123 <lingd123@gmail.com>
2023-03-05 14:25:04 +0800

    test

--- src/main/java/com/example/demo/test.java
+++ src/main/java/com/example/demo/test.java
@@ -1,17 +1,19 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class test {
     
-    @GetMapping("/hello")
+    @RequestMapping("/hello")
     public String name() {
-        return " skldflskadfjasdf";
+        return "<h2> HELLO 87</h2>";
     }
-    @GetMapping("/")
+
+    @RequestMapping("/")
     public String name2() {
-        return "你好啊 旅行者";
+        return "<h1>你好啊 旅行者<h1><a href='/hello'>test</a>";
     }
 }
